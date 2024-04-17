e715c53acdee18d79bed27cb681f0de22d7f22eb
Francesca Correa <francescacciavaglia@gmail.com>
2024-04-17 15:07:58 +0200

    new vite + react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+import data from '../data'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
