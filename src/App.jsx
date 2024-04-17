4b781758b939c0882382ee54a1f0cc4e15352215
Francesca Correa <francescacciavaglia@gmail.com>
2024-04-17 17:02:02 +0200

    import data & render card components with the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 import Header from './components/Header'
+import Card from './components/Card'
 import data from '../data'
 
 function App() {
 
+  const cards = data.map(item =>
+    <Card
+      key={item.id}
+      {...item}
+    />
+  )
+
   return (
     <>
       <Header />
+      {cards}
     </>
   )
 }
