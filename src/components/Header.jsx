7a0e38948bdd995941b3f06e7d84ea3d7997a9b4
Francesca Correa <francescacciavaglia@gmail.com>
2024-04-17 17:00:18 +0200

    import and render logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from '/public/assets/logo.svg'
+
 export default function Header() {
   return (
     <header>
-      <img src="../assets/logo.svg" alt="" />
+      <img src={logo} alt="" />
       <h1>My travel journal.</h1>
     </header>
   )
